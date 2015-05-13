f4b4246b53a87ccd0d7bb38efad0be680f75d8a3
tannerjfco <tannerjfco@gmail.com>
2015-05-12 19:43:44 -0600

    cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,9 +22,6 @@ module.exports = function(grunt) {
       }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-
   grunt.registerTask('default', ['sass']);
 
 };
